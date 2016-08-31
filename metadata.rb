3f0200a6dd288e947b62bdc71aa4dd767e4b7803
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-08-31 14:26:34 +0200

    Cookbook scaffold added.

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'variant_call_rna'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures variant_call_rna'
+long_description 'Installs/Configures variant_call_rna'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/variant_call_rna/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/variant_call_rna' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: variant_call_rna
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
