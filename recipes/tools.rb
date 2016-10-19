3fda383221bacdc54c5a0510fae1e4b14839d0bd
Jörgen Brandt <joergen.brandt@onlinehome.de>
2016-10-19 15:18:25 +0200

    Typo fixed.

--- recipes/tools.rb
+++ recipes/tools.rb
@@ -5,7 +5,7 @@
 # Copyright (c) 2016 Jörgen Brandt, All Rights Reserved.
 
 include_recipe "chef-bioinf-worker::picard"
-incluce_recipe "chef-bioinf-worker::gatk"
+include_recipe "chef-bioinf-worker::gatk"
 include_recipe "chef-bioinf-worker::fastqc"
 include_recipe "chef-bioinf-worker::samtools"
 include_recipe "chef-bioinf-worker::star"
